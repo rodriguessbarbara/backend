0c6b33e182c11fdeb935ddfc32d08d5a69e2a468
barbara <rodriguessbarbara@hotmail.com>
2024-06-14 20:21:44 -0300

    feat: :sparkles: IA] integracao do app com dall-e

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const routes = require("./router");
+const path = require("path");
 
 const port = 8000;
 
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(cors());
 routes(app);
 
